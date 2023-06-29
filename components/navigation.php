8c7bd33c865389d3dd30d04e4beb87d5f0d65227
thiothedev <thiothedev@proton.me>
2023-06-29 17:27:39 +0200

    Making the Page navigation a component

---
+++ components/navigation.php
@@ -0,0 +1,23 @@
+<aside>
+  <h2>Navigace</h2>
+  <ul>
+    <li>
+      <a href="index.php">Domů</a>
+    </li>
+    <li>
+      <a href="c.php">C</a>
+    </li>
+    <li>
+      <a href="cpp.php">C++</a>
+    </li>
+    <li>
+      <a href="python.php">Python</a>
+    </li>
+    <li>
+      <a href="opengl.php">OpenGL</a>
+    </li>
+    <li>
+      <a href="matematika.php">Matematika</a>
+    </li>
+  </ul>
+</aside>
--- python.php
+++ python.php
@@ -11,29 +11,7 @@
   <link rel="manifest" href="/favicon/site.webmanifest">
 </head>
 <body>
-  <aside>
-    <h2>Navigace</h2>
-    <ul>
-      <li>
-        <a href="index.php">Domů</a>
-      </li>
-      <li>
-        <a href="c.php">C</a>
-      </li>
-      <li>
-        <a href="cpp.php">C++</a>
-      </li>
-      <li>
-        <a href="python.php">Python</a>
-      </li>
-      <li>
-        <a href="opengl.php">OpenGL</a>
-      </li>
-      <li>
-        <a href="matematika.php">Matematika</a>
-      </li>
-    </ul>
-  </aside>
+  <?php include('./components/navigation.php'); ?>
   <main>
     <h1>Python</h1>
   </main>
