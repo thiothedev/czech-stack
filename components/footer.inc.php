e6f8d3aba94e4f9882f1807b15779c9783e69b6a
thiothedev <thiothedev@proton.me>
2023-06-29 18:51:31 +0200

    Making the Page footer a component

---
+++ components/footer.inc.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>Czech Stack &copy; 2023</p>
+</footer>
--- index.php
+++ index.php
@@ -4,20 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Czech Stack | Domů</title>
-  <link rel="stylesheet" href="/styles/main.css">
-  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/favicon/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/favicon/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-  <link rel="manifest" href="/favicon/site.webmanifest">
+  <?php include('./include/links.inc.php'); ?>
 </head>
 <body>
-  <?php include('./components/navigation.php'); ?>
+  <?php include('./components/navigation.inc.php'); ?>
   <main>
     <h1>Czech Stack</h1>
   </main>
-  <footer>
-    <hr>
-    <p>Czech Stack &copy; 2023</p>
-  </footer>
+  <?php include('./components/footer.inc.php'); ?>
 </body>
 </html>
