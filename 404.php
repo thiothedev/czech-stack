e01d156efbf2abdd6c100c910807492164466c4d
thiothedev <thiothedev@proton.me>
2023-06-29 21:41:20 +0200

    Moving pages into a custom directory and adding a 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Czech Stack | 404</title>
+  <?php include('./include/links.inc.php'); ?>
+</head>
+<body>
+  <?php include('./components/navigation.inc.php'); ?>
+  <main>
+    <h1>Error 404</h1>
+    <p>Stránka nenalazena.</p>
+  </main>
+  <?php include('./components/footer.inc.php'); ?>
+</body>
+</html>
--- pages/opengl.php
+++ pages/opengl.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Czech Stack | OpenGL</title>
-  <?php include('./include/links.inc.php'); ?>
+  <?php include('../include/links.inc.php'); ?>
 </head>
 <body>
-  <?php include('./components/navigation.inc.php'); ?>
+  <?php include('../components/navigation.inc.php'); ?>
   <main>
     <h1>OpenGL</h1>
   </main>
-  <?php include('./components/footer.inc.php'); ?>
+  <?php include('../components/footer.inc.php'); ?>
 </body>
 </html>
